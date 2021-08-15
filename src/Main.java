858d672809ecd8ce5e7e399ee5143006d8206476
cml87 <gambarimas87@gmail.com>
2021-08-15 21:27:29 +0200

    pass by value, pass by reference !

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,35 @@
 /*
-* - static methods are called class methods. They are normally used to access static fields of their class,
-*   as these cannot be accessed from instance methods (non static methods).
+* - Java always does pass-by-value. When we pass an object to a method, we are passing (by value) its memory location,
+*   which means that, in the method, that parameter will be pointing to the same object as in the caller code.
+*   Therefore we'll actually be able to modify the object inside the called method !
 *
-*   Static, or class, methods cannot access non-static (or instance) members (fields and methods). They must use
-*   an object reference for for this. Similarly they cannot use the 'this' keyword as there is no instance for 'this'
-*   to refer to.
+*   We will be able as well to simply make point the identifier in the method parameter to a new memory location
+*   (with 'new') if desired. This will just change the value of the identifier of the method parameter
+*   (see Fig 6.3 p.215 Pellegrino Principe Java 11).
 *
-* - 'main' is just the method that a Java program executes my default.
+*   When we use an identifier of some object in the arguments of a method call, that identifiers is first replaced
+*   by it's value, which will be a memory address. Then a copy of that value is passed to the parameter in the method.
+*   That's why we cannot actually modify the value of the identifier of the reference type, inside the called method.
+*   In other words, we cannot make that identifier to point (memory address) to another object, inside the
+*   called method. The latter would be equivalent to change the value of the identifier. If we could pass the
+*   memory address where the value of the identifier is stored (which is another memory address), we could modify it,
+*   effectively making that identifier to point to a newly created object. In Java this is not possible. In C/C++ it is.
+*
+*   More:
+*   At low level, either a primitive type variable or reference type variable is nothing more than a named memory
+*   location (memory location of the variable. PP p.215). A primitive type will store in that memory location an integer value,
+*   for example (in binary of course). A reference type instead will store a memory address :), which will be where
+*   the actual object is stored :) In Java, however, we cannot pass to a method the memory location of the variable.
+*   We can only pass what that memory location has :)
+*
+*    See answer by Mike Nakis at
+*   https://softwareengineering.stackexchange.com/questions/286008/parameters-are-passed-by-value-but-editing-them-will-edit-the-actual-object-li/286013
+*
+*   We can avoid modifying the object by passing a reference to a copy of it, instead. We can easily obtain a copy
+*   of the object by properly defining a 'copy constructor'. Copy constructors for classes having reference type
+*   fields will be more complex, as we will need to define a copy constructor for each of these types.
+*
+*   See Pellegrino Principe, Java 11, p. 211-215
 *
 * */
 
@@ -14,7 +37,6 @@
 class Myint {
 
     int x;
-    private static double num;
 
     public Myint() { //if a parametric constructor is defined for a class, the parameterless constructor will
         // no be included by default, so we need to write it
@@ -24,21 +46,9 @@ class Myint {
         this.x = x;
     }
 
-    public void printHello(){
-        System.out.println("Hello");
-    }
-
-    static double getNum(){
-        //System.out.println("value of x is" + String.valueOf(x) );// not ok cuz x is non-static
-        //printHello(); //not ok, cuz method printHello() is non-static
-        return num;
-    }
-
-    static double getNum(Myint myint){
-        System.out.println("value of x is" + String.valueOf(myint.x) );// access non-static x field through an
-        // instance reference
-        myint.printHello(); // access non-static printHello() through an instance reference
-        return num;
+    // a copy constructor
+    public Myint (Myint another){
+        this.x = another.x;
     }
 
 }
@@ -46,25 +56,28 @@ class Myint {
 
 public class Main {
 
+     static void alterMyInt(final Myint myint, int newVal){
+        myint.x = newVal;
+    }
+
     public static void main (String[] args){
 
         Myint myint = new Myint();
         myint.x =3;
 
-        final Myint myint2 = new Myint(5); // a final reference, so it cannot be reassigned
-        myint.x =6;  //this is ok
+        System.out.println(myint.x);
 
-        //myint2 = new Myint(); // this is not ok, cuz the reference myint2 was declared final
+        // pass the object by value (a reference to it) and seeing how the called method modify one of its fields
+        alterMyInt(myint,5);
+        System.out.println(myint.x);
 
-        printMyInt(myint);
+        // now we pass a copy of the object, so the object in the caller code is not modified
+        alterMyInt(new Myint(myint),99);
+        System.out.println(myint.x);
+
 
     }
 
-    static void printMyInt(final Myint myint){
-        myint.x = 4; // this is ok, we are modifying the object pointing to by the reference
-        //myint = new Myint(); // this is not ok, cuz we cannot make the reference point to somewhere else cuz its 'final'
 
-        System.out.println("Myint is: " + myint.x);
-    }
 
 }
