75c707663dd5343409684178fcacaf05ccd6cde1
cml87 <gambarimas87@gmail.com>
2021-08-17 10:58:52 +0200

    method return type casting

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 /*
- * When arguments in a method invocation do not match the type of the corresponding parameter in the method definition,
- * Java will try to make an implicit casting. If it succeeds, it will not complain at all and will execute the method.
- * If it can't, for example because there would be some data lose, it will throw and exception at Runtime.
+ * Implicit casting also takes places with methods return type. If it can be done without data lose Java will do it
+ * without complain. If it can't, it will complain.
+ *
  *
  * */
 
@@ -30,40 +30,23 @@ class Myint {
 
 public class Main {
 
-    public static int sum(int x, int y) { return x + y; }
+    public static double returnDouble() {
+        int x = 4;
+        return x;
+    }
 
+    public static int returnInt() {
+        double y = 4.7;
+        //return y;      //this will not be allowed cuz there is no implicit conversion (without data lose)
+                        // from double to int
+        return (int)y;  //explicit casting
+    }
 
 
     public static void main(String[] args) {
 
-        byte x = 1;
-        byte y = 2;
-
-        int a = 2;
-        float b = 4.f;  // the f, or F, indicates the compiler to interpret this literal as a float
-        double c = 8.78; //or 8.78d or D
-
-        System.out.println("Sum of x and y is: "+sum(x,y)); //3
-
-        // Runtime Error: "public static int sum(int x, int y) { return x + y; }"
-        //System.out.println("Sum of a and b is: "+sum(a,b));
-
-
-        // Notice how expressions passed as arguments are first evaluated, from left to right, to get a value,
-        // and then that value is passed to the function. In the expression evaluation, variables' values can be
-        // changed !
-        x = 5;
-        System.out.println("Sum of x-=5 and x is: "+sum(x-=5,x)); // prints 0. In the first argument expression we make
-        // x==0 and pass that to the method. The second argument will then be zero. 0+0=0
-
-        x=6;
-        System.out.println("\"x-=9\" is: "+(x-=9)); // prints -3
-
-        x=6;
-        y=4;
-        //System.out.println("Sum of x/0 and y-=3 is: "+sum(x/0,y-=3)); //ArithmeticException when evaluating first
-        // argument expression. Second argument expression will not be evaluated, thus 'y' will not be changed
-
+        System.out.println("cast to double from int: " + returnDouble());
+        System.out.println("explcit cast to int from double (data lose): " + returnInt());
     }
 
 }
