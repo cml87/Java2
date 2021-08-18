b49c11517498916e8a06ce3ada1b96f819ae0347
cml87 <gambarimas87@gmail.com>
2021-08-18 13:08:42 +0200

    main() method, command line arguments

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,32 @@
 /*
- * A method can call another method and so on. Method calls can be nested almost "indefinitely". Recursion is the nothing
- * more than nested method calls, using always with the same method.
+ * The main() method is the entry point of a Java program. It is called automatically by the JVM when the program
+ * is run with the java runner command "java".
  *
- * When using a method recursively, it always must have an exit condition or base case, which is a condition that
- * returns a value or exits, instead of calling itself again. Otherwise the recursion will run indefinitely. A recursive
- * algorithm must converge to its base case.
+ * In Java the main() method must always returns 'void'. In other programming languages, such as C and #, it may return
+ * (to the execution environment) an int, eg. 0 to indicate success, or something distinct than 0 (usually 1 or -1) to
+ * indicate failure.
  *
- * Recursion, as a computational algorithm, is equivalent to iteration. Any recursive algorithm can be written using
- * iterations and vice versa. The recursive approach is usually chosen when it is more natural for the problem in hand,
- * though it can be computationally more expensive due to the new memory allocation and handling, needed in each method
- * call.
+ * To communicate success or failure to the execution environment, Java uses 'status code' using the static method
+ * exit() of class java,lang.System, module java.base.
+ *
+
+ * Command-line supplied arguments will be passes to the main() method through its String[] argument, usually called
+ * 'args'. Here we have a simple program that just prints the command line arguments passed. The program will be just
+ * one .class file (Main.class), obtained after compiling the main class of the program. The main class of the program
+ * is the one containing the main() method. We called this class 'Main' here, but it can have any name of course.
+ * We can run this program with:
+ * $java Main one two three
  *
  * */
 
-
-class Myint {
-
-    int x;
-
-    public Myint() { //if a parametric constructor is defined for a class, the parameterless constructor will
-        // no be included by default, so we need to write it
-    }
-
-    public Myint(int x) {
-        this.x = x;
-    }
-
-    // a copy constructor
-    public Myint(Myint another) {
-        this.x = another.x;
-    }
-
-}
-
-
 public class Main {
-
-    public static long factorial(final long n) {
-        // This function doesn't check for positive input, neither for too large values,
-        // eg. 20! is too large and cannot be stored in a long variable
-        if (n == 1)
-            return 1;
-        else
-            return n*factorial(n-1);
-    }
-
-
 
     public static void main(String[] args) {
 
-        long n=4;
-        System.out.printf("Factorial of %d is %d: ",n, factorial(n));
+        System.out.println("The command line arguments are: ");
+        for (int i=0; i < args.length; i++ ){
+            System.out.println(args[i]);
+        }
 
     }
 
