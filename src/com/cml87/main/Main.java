a0571058052aeb2e32f0df44dd22005ac8d9a6ca
cml87 <gambarimas87@gmail.com>
2021-08-20 13:07:40 +0200

    jar 1

--- src/com/cml87/main/Main.java
+++ src/com/cml87/main/Main.java
@@ -1,14 +1,39 @@
 package com.cml87.main;
 
-import com.cml87.utils.Myutils;  // other package, so we need to import this class
 /*
+ * See Java Tools Tutorials - Herong's Tutorial Examples
+ * See jar --help
  *
- * Classes in the same package as the package of the class from where we are using them, do not need import.
- * From the directory having the base package 'com' we can run this program as
- * java com.cml87.main.Main bim piu
+ * JAR - Java Archive File Format
+ * Java applications can be stored in jar files (Java archive). It can bundle many .class files, images, sounds.
  *
+ * It is a ZIP file.
  *
+ * Can have attributes stored in the manifest file .../META-INF/MANIFEST.INF.
  *
+ * A jar file can be used in classpath for javac and java commands, if we can to include its classes.
+ *
+ * It can be executable if 'Main-Class' is provided in its manifest.
+ *
+ *  It can be a module JAR, if module-info.class is provided in its root directory (?)
+ *
+ *  We can pass options to the jar command through a file using '@' eg:
+ *  jar --create --file my.jar @classes.list
+ *
+ *  We can compile the class and add it to a jar with. The manifest file (META-INF/MANIFEST.MF) will be added
+ *  automatically.
+ *  $ javac Main.java  // creates a .class in the current dir
+ *  $ jar --create --verbose --file main.jar Main.class  // creates the jar with our class
+ *
+ *  In short format the above jar command can be written as
+ *  $ jar cvf main.jar Main.class
+ *  $ jar -c -v -f main.jar Main.class
+ *  $ jar -cvf main.jar Main.class
+ *
+ *  We can list a jar content with:
+ *  $jar --list -v -f main.jar // or tvf
+ *
+ *  To extract the files in a jar: jar --extract --verbose --file <jar_file>  // or xvf
  * */
 
 
@@ -16,16 +41,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        StringBuilder sb = new StringBuilder(10);
-
-        for (int i=0; i < args.length; i++ ){
-            sb.append(args[i]);
-        }
-
-        System.out.println("The command line arguments are (removing spaces): ");
-        Myutils.print(sb.toString());
-
-        Dev.aux(); //method of the same package. It doesn't need import
+        System.out.println("Hello jar !");
 
     }
 
