520dd7abe119533f033e6e9a98eacbab8f9eb98f
cml87 <gambarimas87@gmail.com>
2021-08-27 11:22:11 +0200

    jar manifest file

--- src/com/cml87/main/Main.java
+++ src/com/cml87/main/Main.java
@@ -3,33 +3,59 @@ package com.cml87.main;
 import com.cml87.utils.Myutils;
 
 /*
- * We create a jar file for a multi-package java application with option -C
  *
- * $ jar --create --verbose --file main.jar -C /home/camilo/my_java_projects/Java2/out/production/Java2/ ./com
- * $ jar cvf main.jar -C /home/camilo/my_java_projects/Java2/out/production/Java2/ ./com
- * where
+ * A manifest in a jar is a file named META-INF/MANIFEST.MF. It contains attributes about the jar file and its contents.
+ * Attributes will be a list of name-value pairs in the format "name: value\n".
  *
- * /home/camilo/my_java_projects/Java2/out/production/Java2/ -> tells jar to change the working dir to here
- * ./com  -> archive the entire subdirectory tree of ./com, which is the base package of our application. In this tree
- * we should have the compiled .class files of or application, inside the corresponding package directories.
+ * The list is divided into a main section for package-level attributes, and multiple subsections, for entry-level
+ * attributes (?). Each attribute must be ended with a new line character.
  *
- * The manifest file will be created automatically. Its content can be seen with
+ * The main section may contain only the package level attributes like: Manifest-Version, Created-By, Main-Class,
+ * Class-Path etc.
  *
- * $ jar tvf main.jar // --list --verbose --file
+ * Subsections are optional. They are preceded with a blank line and start with attribute "Name".
  *
- *  The generated jar file will be saved in the current dir. Extracting the jar file with
+ * There are two ways to create a manifest file:
+ * 1. Automatically create the manifest file but pass attributes to it from a text file.
  *
- * $ jar xvf main.jar // --extract --verbose --file
+ *  $ jar cvf main.jar --manifest=fileWithAttributes.txt -C /home/camilo/my_java_projects/Java2/out/production/Java2/ ./com
+ *  $ jar cvfm main.jar fileWithAttributes.txt -C /home/camilo/my_java_projects/Java2/out/production/Java2/ ./com
+ *   //cvf -> --create --verbose --file
+ *   // --manifest=fileWithAttributes.txt  -> -m fileWithAttributes.txt  or m
+ *  The file fileWithAttributes.txt will contain two line. The first will be Main-Class: com.cml87.main.Main. The second
+ *  line will be empty. Ie. we are specifying where is the class containing the main() method.
+ *  Now, instead of running the file with
  *
- * We can go and examine the META-INF/MANIFEST.MF and see that it doesn't contain a Main-Class entry. Therefore, to
- * run this jar as an application (run its main() method) we need to tell jar which class contains the main() method.
- * We use option cp:
+ *  $ java -cp main.jar com.cml87.main.Main  //specify the class path of the main class
  *
- * $  java -cp main.jar com.cml87.main.Main
+ *  we can just do
  *
- * Next we'll see how to specify custom entries to the manifest file, or how to pass a complete manifest file to the jar
- *  command.
+ *  $ java -jar main.jar  // the location of the main class is in the manifest file
  *
+ *  2. Explicitly specify the manifest file to use. Eg. I have created the file META-INF/MANIFEST.MF with content
+ *
+ *    Manifest-Version: 1.0\n
+ *    Main-Class: com.cml87.main.Main\n
+ *    Created-By: The Avengers in Infinity War\n
+ *
+ *  Then I tell jar to use it for the manifest file with (I specify only the location of META-INF/ directory):
+ *
+ *  $ jar cvfM main.jar META-INF/ -C /home/camilo/my_java_projects/Java2/out/production/Java2/ ./com
+ *
+ *  // --no-manifest -> M
+ *
+ *  I also tried
+ *
+ *  $ jar cvf main.jar --no-manifest META-INF/ -C /home/camilo/my_java_projects/Java2/out/production/Java2/ ./com
+ *
+ *  which should be the same, but it didn't work.
+ *
+ *
+ *
+ *
+ *
+
+ * 2.
  *
  * */
 
