59a7894bf51b74446bd2d6d920e2589a200a25dd
cml87 <gambarimas87@gmail.com>
2021-08-28 19:30:33 +0200

    jar other thoughts

--- src/com/cml87/main/Main.java
+++ src/com/cml87/main/Main.java
@@ -3,25 +3,16 @@ package com.cml87.main;
 import com.cml87.utils.Myutils;
 
 /*
- * With the IDE Intellij, it is possible to get the jar file of our project automatically when we build it,
- * including a manifest file. Do:
- * 1. File/Project Structure/Project Settings/Artifacts/+/JAR/From modules with dependencies ...
- * 2. Specify the package qualified class containing the main() method, so Intellij can include it in the
- *    manifest file eg:
- *    com.cml87.main.Main
- * 3. select "extract to the target JAR". Let META-INF/MANIFEST.MF in src/
- *
- * IntelliJ will then automatically create a .../src/META-INF/MANIFEST.MF manifest file. Note that src/ is the directory
- * containing the base package of our project. With a project build now Intellij will create the project jar at
- * .../out/artifacts/ProjectName_jar/ProjectName.jar file.
- *
- * Note that /out is at the same level as src/.
- *
- * We run the jar now with
- * $java -jar /home/xxx/my_java_projects/Java2/out/artifacts/Java2_jar/Java2.jar
- *
- *
- * Real world Java applications will have multiple jar files that will depend on each other.
+ * Other thoughts about jas:
+ * 1. jar files can be specified in Java class path as to the compiler 'javac' and the launcher 'java'
+ *    See Herong, Using JAR files in Java Class Paths
+ * 2. .class files inside a jar can be updated or replaced with option --update.
+ *    See Herong, Updating Class Files in JAR.
+ * 3. We could simulate an executable jat file by making and executable bash script that receive some argumants and
+ *    pass them to a jar file it calls inside with java -jar.
+ *    See 'How to convert “jar” to Linux executable file?' Stackoverflow
+ * 4. We can create a jar to store all class files of a Java module.
+ *    See Herong, 'Creating Module JAR File'
  *
  * */
 
