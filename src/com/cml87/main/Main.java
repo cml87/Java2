0f0dbc33a3bffd6b391458daf21d46557b01c5cb
cml87 <gambarimas87@gmail.com>
2021-08-30 22:15:22 +0200

    class initializer block

--- src/com/cml87/main/Main.java
+++ src/com/cml87/main/Main.java
@@ -2,35 +2,24 @@ package com.cml87.main;
 
 import com.cml87.utils.Myutils;
 
-/*
- * Method overloading is defining two methods with the same name a given class, but with different signatures.
- *
- * In Java a method signature is the combination of the method name, its number of parameters, the type of parameters
- * and order of parameters. Java cannot distinguish between two methods with the same signature. Therefore, method
- * overloading is only allowed for methods that differ in their signature somehow.
- *
- * The return type, method modifiers, and thrown exceptions do not form part of the method signature.
- *
- * Method overloading is used to define methods that have very similar logic but differ in some details. Sometimes
- * 'generics' can be used instead of method overloading, avoid to define several methods with the same name.
- *
- * Method overloading is commonly used with class constructors.
- *
- * Method overloading is one of the three types of polymorphisms that exist in Java: static polymorphism.
- * It's is static because at compile time the compiler will know which method version to use, performing an 'early
- * binding' between the caller code and the method.
- *
- * The other two types of polymorphism are 'subtype polymorphism' (OOP and inheritance) and 'parametric polymorphism'
- * (generics)
- *
- * */
-
-
 public class Main {
 
-    // Two methods with the same signature
-    // public void foo( double d){}
-    // public int foo(double d){}
+    private int x;
+
+    private static String str;
+
+    private final int y;
+
+    // an instance initializer block to initialize instance variables
+    {
+        //x =7;
+        y = 3;  //variable y must be initialized cuz it's final
+    }
+
+    // a static initializer block to initialize static fields
+    static {
+        str = "Hello!";
+    }
 
     public static void main(String[] args) {
 
