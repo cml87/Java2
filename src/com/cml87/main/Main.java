6c7d805829586ccf8405fc4311e0cc3b52e83ac7
cml87 <gambarimas87@gmail.com>
2021-09-05 00:08:22 +0200

    class finalizer

--- src/com/cml87/main/Main.java
+++ src/com/cml87/main/Main.java
@@ -2,61 +2,48 @@ package com.cml87.main;
 
 import com.cml87.utils.Myutils;
 
-class Classb{
-     private int x =4;
+class FileManager {
 
-     // toString() method overwriting superclass Object.toString() method
-     public String toString(){
-         return String.valueOf(x);
-     }
+    public FileManager(){
+        System.out.println("FileManager constructor invoked ...");
+    }
 
-     public Classb (int x){
-         this.x = x;
-     }
+    protected void finalize() {
+        System.out.println("FileManager finalizer invoked ...");
+    }
 
-    // See how the private field of type Classb can be accessed directly in this constructor
-    // and the method below. It's because this constructor and this method belong to the same class
-    // type
-     public Classb (Classb cb) {
-         this.x = cb.x;
-     }
-     public void methodA(Classb cb){
-         int y = cb.x;
-     }
 }
 
-// This class with the default parameterless constructor will not be allowed
-// cuz it would call the parameterless constructor of its superclass which
-// doesn't exist
-// class Classc extends Classb {}
 
 
 public class Main {
 
-    private int x;
-
-    private static String str;
-
-    private final int y;
-
-    // an instance initializer block to initialize instance variables
-    {
-        //x =7;
-        y = 3;  //variable y must be initialized cuz it's final
-    }
-
-    // a static initializer block to initialize static fields
-    static {
-        str = "Hello!";
-    }
-
     public static void main(String[] args) {
 
-        Myutils myutils0;
+        FileManager fm = new FileManager();
 
-        // We cannot instantiate an object of class Myutils cuz the constructor we are calling with new
-        // has 'default' visibility (ie. only in package). There it's not visible here, we're in another package.
-        //Myutils myutils = new Myutils(4);
+        fm = null; // now the memory previously allocated with new will be eligible for garbage-collection
+
+        // totalMemory(): returns the amount of memory currently allocated, plus the available memory to allocate more
+        // objects in the future, always in bytes
+        // freeMemory(): returns only the memory available, to be allocated for new objects in the future, in bytes.
+        System.out.printf("Memory occupied in the heap, in bytes: %d%n",
+                Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
+
+        // kindly ask the JVM  to start-up garbage collector operations ??
+        // kindly ask because we cannot explicitly call the garbage collector actually. It seems we can only hint the
+        // JVM to do it
+        Runtime.getRuntime().gc();
+
+        System.out.println("- a -");
+
+        // kindly ask the JVM to call the finalizers of the objects eligible for garbage-collection ??
+        Runtime.getRuntime().runFinalization();
+
+        System.out.println("- b -");
+
+        System.out.printf("Memory occupied in the heap, in bytes: %d%n",
+                Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
 
 
     }
