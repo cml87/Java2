703cead553634892b2f45859b1481d3fdbdb75ec
cml87 <gambarimas87@gmail.com>
2021-09-15 12:02:10 +0200

    local inner class, access to outer method local variables, including method parameters

--- src/com/cml87/main/InnerClassTest.java
+++ src/com/cml87/main/InnerClassTest.java
@@ -11,8 +11,8 @@ import javax.swing.Timer;
 public class InnerClassTest {
 
     public static void main(String[] args) {
-        TalkingClock clock = new TalkingClock(1000, true);
-        clock.start();
+        TalkingClock clock = new TalkingClock();
+        clock.start(1000, true);
 
        //keep the program running until user selects "OK"
         JOptionPane.showMessageDialog(null, "Quit program?");
@@ -28,21 +28,17 @@ A clock that prints the time at regular intervals
 
 // Outer class
 class TalkingClock {
-    private int interval;
-    private boolean beep;
 
-    public TalkingClock(int interval, boolean beep){
-        this.interval = interval;
-        this.beep = beep;
-    }
 
-    public void start(){
+    //when this method is called, the compiler will automatically call the constructor of the inner class passing the
+    //method arguments to set the corresponding fields, also created automatically by the compiler.
+    public void start(int interval, boolean beep){
 
         // local inner class
         class TimePrinter implements ActionListener{
             int x;
-            //the compiler will insert a TalkingClock reference (outer class) and a 'beep' field for all constructors of
-            // this inner class
+            //the compiler will insert a TalkingClock reference (outer class) and a 'interval' and 'beep' field for all
+            // constructors of this inner class
             public TimePrinter(){}
 
             private TimePrinter(int x){ this.x=x;  }
@@ -52,8 +48,8 @@ class TalkingClock {
 
                 // access to a private field of the outer class instance that creates this instance,
                 // without a getter method !!
-                if (TalkingClock.this.beep)
-                //if (beep)
+                //if (TalkingClock.this.beep)
+                if (beep)
                     Toolkit.getDefaultToolkit().beep();
             }
         }
