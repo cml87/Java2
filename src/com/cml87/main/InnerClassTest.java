4e87d839feacc47f31fe46a328c75b791d5e6db0
cml87 <gambarimas87@gmail.com>
2021-09-15 11:42:04 +0200

    add notes

--- src/com/cml87/main/InnerClassTest.java
+++ src/com/cml87/main/InnerClassTest.java
@@ -52,26 +52,21 @@ class TalkingClock {
 
     }
 
+
     // Inner class
     public class TimePrinter implements ActionListener{
-
         int x;
-
         //the compiler will insert a TalkingClock reference (outer class) as a parameter for this constructor
-        public TimePrinter(){
+        public TimePrinter(){}
 
-        }
-
-        private TimePrinter(int x){
-            this.x=x;
-        }
+        private TimePrinter(int x){ this.x=x;  }
 
         public void actionPerformed(ActionEvent event){
             System.out.println("At the tone the time is "+ new Date());
             //if (TalkingClock.this.beep)
-            if (beep)   //  access to a private field of the outer class without a getter method !!
+            if (beep)   //  access to a private field of the outer class instance that creates this instance,
+                        // without a getter method !!
                 Toolkit.getDefaultToolkit().beep();
         }
-
     }
 }
