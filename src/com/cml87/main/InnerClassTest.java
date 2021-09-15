26240c0eb1305c65941af2e71f278140c9680fd1
cml87 <gambarimas87@gmail.com>
2021-09-15 13:27:30 +0200

    anonymous class

--- src/com/cml87/main/InnerClassTest.java
+++ src/com/cml87/main/InnerClassTest.java
@@ -34,28 +34,18 @@ class TalkingClock {
     //method arguments to set the corresponding fields, also created automatically by the compiler.
     public void start(int interval, boolean beep){
 
-        // local inner class
-        class TimePrinter implements ActionListener{
-            int x;
-            //the compiler will insert a TalkingClock reference (outer class) and a 'interval' and 'beep' field for all
-            // constructors of this inner class
-            public TimePrinter(){}
-
-            private TimePrinter(int x){ this.x=x;  }
-
+        // local anonymous inner class
+        ActionListener listener = new ActionListener(){
             public void actionPerformed(ActionEvent event){
                 System.out.println("At the tone the time is "+ new Date());
-
                 // access to a private field of the outer class instance that creates this instance,
                 // without a getter method !!
                 //if (TalkingClock.this.beep)
                 if (beep)
                     Toolkit.getDefaultToolkit().beep();
             }
-        }
+        };
 
-        //ActionListener listener = this.new TimePrinter();
-        ActionListener listener = new TimePrinter();
         Timer t = new Timer(interval, listener);
         t.start();
 
